4ac0ed813d171674df797786691be7a63775c58d
Kamil Oczkowski <kamil@oczkow.ski>
2020-10-04 14:34:29 +0100

    MongoDB connection function done, readme updates

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ A small test application allowing the user to create multi path stories utilisin
 -   MongoDB running instance, locally or remotely
 -   Node 10 or above
 -   At least one network adapter and connect Mongo to the application
--   (Optional) nodemon, alternatively run `npm start` to start the application.
 
 ### Installation instructions
 
@@ -22,3 +21,8 @@ A small test application allowing the user to create multi path stories utilisin
    --- users
 6. Make sure your machine's port 3000 isn't occupied and run `npm start`, if for any reason that port is used please change it in app.js
 7. To view and interact with the application, visit http://localhost:3000, change port if other was specified.
+
+### Optional features
+
+-   For easier development and running the application install `nodemon`.
+-   For security between the server and MongoDB, you can setup database credentials in the `mongo.js` file.
--- mongo.js
+++ mongo.js
@@ -5,3 +5,35 @@ const { connect } = require('mongodb');
  * All mongo settings are here, since it's a small application
  * there is no need for a .env file.
  */
+
+// Settings
+const HOSTNAME = '0.0.0.0';
+const PORT = 27017;
+const DATABASE_NAME = 'MultiPathStoryMaker';
+
+// Optional Authentication
+const USERNAME = '';
+const PASSWORD = '';
+
+// Create mongo connection
+const authString = USERNAME && PASSWORD ? `${USERNAME}:${PASSWORD}@` : '';
+const mongoURL = `mongodb://${authString}${HOSTNAME}:${PORT}`;
+
+// Connect function - Returns an instance of conenction to MongoDB Database
+async const connectToDatabase = () => new Promise((resolve, reject) => {
+    connect(mongoURL, { useUnifiedTopology: true }, function(error, client) {
+        // Connection error
+        if(error){
+            console.log(`Error whilst connecting to MongoDB, ${err}`);
+            reject(error);
+        }
+        
+        // Connection successful
+        console.log(`Connected to MongoDB at ${HOSTNAME}:${PORT} successfull!`);
+        const DATABASE = client.db(DATABASE_NAME);
+        resolve(DATABASE)
+    })
+});
+
+// Export function
+module.exports = connectToDatabase;
