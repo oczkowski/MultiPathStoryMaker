744f85f3ecd7209be76053f7b2d91ffbf4d1250e
Kamil Oczkowski <kamil@oczkow.ski>
2020-10-04 21:27:29 +0100

    Chaining stories now available

--- routes/createStory.js
+++ routes/createStory.js
@@ -24,7 +24,7 @@ router.post('/new', async function createNewStory(request, result) {
 
     // If no title or empty, redirect back
     if (title === undefined || title.length === 0)
-        request.redirect('/createStory/new');
+        result.redirect('/createStory/new');
 
     // Create story in MongoDB
     const mongoDatabase = await connectToDatabase();
@@ -51,7 +51,7 @@ router.post('/new', async function createNewStory(request, result) {
 });
 
 // Story building process
-router.get('/build/:storyID/:currentPath', async function createNewStory(
+router.get('/build/:storyID/:currentPath', async function buildStory(
     request,
     result
 ) {
@@ -72,23 +72,104 @@ router.get('/build/:storyID/:currentPath', async function createNewStory(
         : false;
 
     // Does story exist?
-    if (!story) {
+    if (!storyInstance) {
         result.redirect('/createStory/new');
     }
 
     // Access current story path
     const pathArray = currentPath.split('.').slice(1); // We remove the first one as it's the root access
-    var currentObject = storyInstance.story;
+    var currentObject = storyInstance;
     do {
-        let propertyForAccess = pathArray.shift();
-        currentObject =
-            propertyForAccess !== undefined
-                ? currentObject[propertyForAccess]
-                : currentObject;
+        let propertyForAccess = pathArray.shift(); // For the purpose of this application this value will be 0, 1, 2 or 3.
+
+        // We hit last object access, let's get what we need
+        if (propertyForAccess !== undefined) {
+            currentObject = currentObject.story[propertyForAccess];
+        }
     } while (pathArray.length);
 
+    var currentStory = currentObject.story;
+    var currentTitle = currentObject.title;
+
+    console.log(currentObject);
+    console.log(currentTitle);
+    console.log(currentStory);
+
     // Once created, redirect to builder passing in the story ID
-    result.render('createStory/build', { storyID, currentPath });
+    result.render('createStory/build', {
+        storyID,
+        currentPath,
+        currentTitle,
+        currentStory,
+    });
+});
+
+// Save new story property
+router.post('/build/:storyID/:currentPath', async function buildStory(
+    request,
+    result
+) {
+    // Get story details
+    const { storyID, currentPath } = request.params;
+
+    // Get story content to save
+    const { story0, story1, story2, story3 } = request.body;
+
+    // What story index are we inserting on?
+    var storyIndex;
+    if (story0) storyIndex = 0;
+    else if (story1) storyIndex = 1;
+    else if (story2) storyIndex = 2;
+    else if (story3) storyIndex = 3;
+
+    // We need at least one story passed in order to save, if none exist let's redirect
+    if (!story0 && !story1 && !story2 && !story3)
+        result.redirect(`/build/${storyID}/${currentPath}`);
+
+    // Let's try looking for a story with that ID
+    const mongoDatabase = await connectToDatabase();
+    const stories = mongoDatabase.collection('stories');
+
+    const storyInstance = storyID
+        ? await stories.findOne({ _id: ObjectID(storyID) })
+        : false;
+
+    // Does story exist?
+    if (!storyInstance) {
+        result.redirect('/createStory/new');
+    }
+
+    // Add new story to object
+    const pathArray = currentPath.split('.').slice(1); // We remove the first one as it's the root access
+    var currentObject = storyInstance;
+    var currentStory;
+    var currentTitle;
+    do {
+        let propertyForAccess = pathArray.shift(); // For the purpose of this application this value will be 0, 1, 2 or 3.
+
+        // We hit last object access, Let's update the story
+        if (propertyForAccess !== undefined) {
+            currentObject = currentObject.story[propertyForAccess];
+        }
+    } while (pathArray.length);
+
+    // Once we get to the correct deph of the object, we update it
+    currentObject.story = {
+        ...currentObject.story,
+        [storyIndex]: {
+            title: story0 || story1 || story2 || story3,
+            story: {},
+        },
+    };
+
+    // Update mongo with new object
+    await stories.updateOne(
+        { _id: ObjectID(storyID) },
+        { $set: { story: storyInstance.story } }
+    );
+
+    // Redirect back to building
+    result.redirect(`/createStory/build/${storyID}/${currentPath}`);
 });
 
 // Return route set
